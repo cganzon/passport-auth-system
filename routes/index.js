f9d39b7128dec2eef11805b6c5c5dcf7d5c9cb6a
crganzon@gmail.com <cezarganzon@cezars-mbp.attlocal.net>
2019-06-13 00:12:46 -0700

    styled initial page and added buttons to link to register and login page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("This is the main page");
+    res.render("welcome");
 })
 
 module.exports = router;
