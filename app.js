ec4d5e23fa89bb8e671c0e59cc87ab0c409d5f77
cganzon <crganzon@gmail.com>
2019-06-14 22:30:33 -0700

    validating user sign up and database configuration

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Setting up express to use json and set it to req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// DB connection
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/usersDB", { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.error(err));
 
 // EJS
 app.use(expressLayouts);
--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,36 @@ router.get("/signup", (req, res) => {
     res.render("signup");
 });
 
+// Sign up handling
+router.post("/signup", (req, res) => {
+    // console.log(req.body);
+    const {name, email, password, password2} = req.body;
+    let errors = [];
+    // Check required fields
+    if(!name || !email || !password || !password2) {
+        errors.push({
+            msg: "Please fill in all fields"
+        });
+    };
+    // Checking if the passwords match
+    if(password != password2) {
+        errors.push({
+            msg: "Passwords do not match!"
+        });
+    };
+    // Check if password is at least 6 characters long
+    if(password.length < 6) {
+        errors.push({
+            msg: "Password must be at least six characters"
+        });
+    };
+    if(errors.length > 0) {
+        res.render("signup", {
+            errors, name, email, password, password2
+        });
+    } else {
+        res.send("Valid");
+    };
+});
+
 module.exports = router;
