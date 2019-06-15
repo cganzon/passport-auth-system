35cfe0062e30679f288af6246737968f91e43005
cganzon <crganzon@gmail.com>
2019-06-15 15:11:35 -0700

    login authentication and logout functionality

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
 const session = require("express-session");
+const passport = require("passport");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Passport config
+require("./config/passport")(passport);
 
 // Setting up express to use json and set it to req.body
 app.use(express.json());
@@ -21,6 +25,10 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect flash
 app.use(flash());
 
@@ -28,6 +36,7 @@ app.use(flash());
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash("success_msg");
     res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
     next();
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const passport = require("passport");
 const User = require("../models/User");
 
 // Login Route
@@ -79,4 +80,19 @@ router.post("/signup", (req, res) => {
     };
 });
 
+// Login handle
+router.post("/login", (req, res, next) => {
+    passport.authenticate("local", {
+        successRedirect: "/dashboard",
+        failureRedirect: "/users/login",
+        failureFlash: true
+    })(req, res, next);
+});
+
+// Logout handle
+router.get("/logout", (req, res) => {
+    req.logout();
+    req.flash("success_msg", "You are logged out");
+    res.redirect("/users/login");
+});
 module.exports = router;
